19834684ac79af9b85399d9d116570c27b01e245
Frédéric Osterrath <frederic.osterrath@mila.quebec>
2020-03-19 16:26:00 -0400

    WIP

--- deepgroove_web_user_experiment/deepgroove_web_user_experiment.py
+++ deepgroove_web_user_experiment/deepgroove_web_user_experiment.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template, request, session, redirect, url_for
 from logging import getLogger, basicConfig, DEBUG
+from uuid import uuid4
 
 basicConfig(level=DEBUG)
 
@@ -9,7 +10,6 @@ APP = Flask(__name__)
 APP.secret_key = b"1qaz2wsx42!000077777"
 
 # TODO : State machine
-# TODO : Renomer la page
 # TODO : Table de ratings
 # TODO : Interface for call to training
 # TODO : Use a process pool with shared object for training.
@@ -24,9 +24,10 @@ def register():
     if request.method == 'POST':
         session['participant_name'] = request.form['participant_name']
         logger.debug("participant name %s", session['participant_name'])
+        session['ratings_table'] = {}
         return redirect(url_for('trial'))
 
-    return render_template('greetings.html')
+    return render_template('landing.html')
 
 
 @APP.route("/trial", methods=['GET', 'POST'])
@@ -41,11 +42,13 @@ def trial():
     logger = getLogger(__name__)
 
     if request.method == 'POST':
-        # TODO : Here we need to connect with a given ID for the sample.
-        vote = request.form['action']
-        # TODO : Call feedback function to model.
+        rating = request.form['action']
+        session['ratings_table'][rating] = request.form['id']
+        # TODO : Trigger model training
         logger.debug("user voted %s", vote)
         return redirect(url_for('trial'))
-    else:
-        session['clip_path'] = url_for('static', filename='trumpet-1.wav')
-        return render_template('trial.html')
+
+    # TODO : Here we should generate the temporary file with a prefix for the generated files.
+    # TODO : Generate a unique UUID for the file.
+    clip_url = url_for('static', filename='trumpet-1.wav')
+    return render_template('trial.html', clip_url=clip_url)
---
+++ deepgroove_web_user_experiment/training_interface.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from wave import open as wav_open
+
+def generate_clip(filename):
+    """
+    Create audio and fill in audio WAV file.
+
+    :param filename: Path to the WAV which should be filled.
+    """
+    # As a placeholder we are only now reading a static file and returning a
+    # bytestream..
+    audio_src = wav_open('static/trumpet-1.wav')
+
+    # samplerate = 44100
+    # nb_channels = 1
+    # bit_depth = 16
+    out_file = Path(filename)
