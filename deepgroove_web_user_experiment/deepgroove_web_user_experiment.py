9cb07453b647c5bd52b8827acfb96bb775838542
Frédéric Osterrath <frederic.osterrath@mila.quebec>
2020-03-19 17:20:43 -0400

    WIP

--- deepgroove_web_user_experiment/deepgroove_web_user_experiment.py
+++ deepgroove_web_user_experiment/deepgroove_web_user_experiment.py
@@ -1,15 +1,13 @@
 """Main WEB interface definition module."""
 
-from flask import Flask, render_template, request, session, redirect, url_for
-from logging import getLogger, basicConfig, DEBUG
-from uuid import uuid4
+from tempfile import NamedTemporaryFile
+from logging import getLogger
+from pathlib import Path
 
-from training_interface import run_train, generate_clip
+from flask import render_template, request, session, redirect, url_for
 
-basicConfig(level=DEBUG)
-
-APP = Flask(__name__)
-APP.secret_key = b"1qaz2wsx42!000077777"
+from .training_interface import run_train, generate_clip
+from . import APP
 
 # TODO : State machine, implement steps
 
@@ -45,10 +43,13 @@ def trial():
         clip_id = request.form['id']
         logger.debug("user said %s of %s", rating, clip_id)
         session['ratings_table'][rating] = clip_id
-        # TODO : Trigger model training through a multi-process pool
+        run_train(session['ratings_table'])
         return redirect(url_for('trial'))
 
     # TODO : Here we should generate the temporary file with a prefix for the generated files.
+    clip_f = NamedTemporaryFile(prefix='./static/', delete=False)
+    clip_path = Path(clip_f.name)
+    clip_id = generate_clip(clip_path)
     # TODO : Generate a unique UUID for the file.
-    clip_url = url_for('static', filename='trumpet-1.wav')
-    return render_template('trial.html', clip_url=clip_url)
+    clip_url = url_for('static', filename=clip_path.stem)
+    return render_template('trial.html', clip_url=clip_url, clip_id=clip_id)
--- deepgroove_web_user_experiment/training_interface.py
+++ deepgroove_web_user_experiment/training_interface.py
@@ -1,26 +1,48 @@
+"""
+Interface code for WEB UI and training setup.
+"""
+
+# from wave import open as wav_open
+from logging import getLogger
 from pathlib import Path
-from wave import open as wav_open
+from uuid import uuid4
 
-def generate_clip(filename):
+def generate_clip(out_file_path):
     """
-    Create audio and fill in audio WAV file.
+    Create audio and fill in audio specified WAV file.
 
-    :param filename: Path to the WAV which should be filled.
+    :param out_file_path: Path to the WAV which should be filled.
+    :returns: The identifier of the clip.
+    :rtype: str
     """
+    logger = getLogger(__name__)
     # As a placeholder we are only now reading a static file and returning a
     # bytestream..
-    audio_src = wav_open('static/trumpet-1.wav')
+    dumb_audio_path = Path('static/trumpet-1.wav')
+    logger.warning("Using stub data for audio generation from file %s",
+                   dumb_audio_path)
+    wav_data = dumb_audio_path.read_bytes()
 
+    out_file_path = Path(out_file_path)
+    logger.debug("Placing audio data into file %s", out_file_path)
+    out_file_path.write_bytes(wav_data)
+
+    # TODO : Create WAV file for the generated audio having these parameters:
+    # (use wave standard library module)
     # samplerate = 44100
     # nb_channels = 1
     # bit_depth = 16
-    out_file = Path(filename)
+
+    # TODO : Connect to the actual ID from the training setup
+    clip_id = uuid4()
+
+    return clip_id
 
 
 def run_train(ratings):
     """
     This launches the training algorith with a (possibly) updated ratings table.
     """
+    logger = getLogger(__name__)
+    logger.debug("processing user submitted ratings : %s", ratings)
     # TODO : Use a process pool with shared object for training.
-    pass
-
